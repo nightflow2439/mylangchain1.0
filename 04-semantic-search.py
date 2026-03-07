ee4ff84f0918caef083ff4ca682767ad48e82f38
river <3522672439@qq.com>
2026-03-07 18:54:42 +0800

    04-2

--- 04-semantic-search.py
+++ 04-semantic-search.py
@@ -19,3 +19,41 @@ results = vector_store.similarity_search(
 for index, result in enumerate(results):
   print(index)
   print(result.page_content[:100])
+
+# 带分数的相似度查询
+results = vector_store.similarity_search_with_score(
+  "What immitations does Force-aware have?"
+)
+
+for doc, score in results:
+  print(score)
+  print(doc.page_content[:100])
+
+# 用向量进行相似度查询
+print("用向量进行相似度查询")
+vector = embedding.embed_query(
+  "What immitations does Force-aware have?"
+)
+
+results = vector_store.similarity_search_by_vector(
+  vector
+)
+
+for index, result in enumerate(results):
+  print(index)
+  print(result.page_content[:100])
+
+# chain: langchain: 大模型，提示词模板，toots，output，Runnable
+print("用检索器进行相似度查询")
+from typing import List
+from langchain_core.documents import Document
+from langchain_core.runnables import chain
+
+@chain
+def retriever(query: str) -> List[Document]:
+  return vector_store.similarity_search(query, k=1)
+
+results = retriever.invoke("What immitations does Force-aware have?")
+for index, result in enumerate(results):
+  print(index)
+  print(result.page_content[:100])
